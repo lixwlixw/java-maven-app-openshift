cc79552113393efba6f1b1fd51cb6cf7d6f18a0f
lixwlixw <871810908@qq.com>
2018-05-14 12:06:50 +0800

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,11 +3,17 @@ package hello;
 
 public class Application {
 
-	public static void main(String[] args) {
-		while (true) {
-			System.out.println("Hello World");
-			Thread.sleep(1000);
-		}
-	}
+ public static void main(String[] args) {
+  while (true) {
+   System.out.println("Hello World");
+
+   try {
+    Thread.sleep(1000);
+   } catch (InterruptedException e) {
+    e.printStackTrace();
+   }
+
+  }
+ }
 
 }
